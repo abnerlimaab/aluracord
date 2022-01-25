43cb9a4690dc3e241d706ac93875e3ce3d16ab93
Abner.Lima <abnerlima.1997@gmail.com>
2022-01-25 20:50:55 -0300

    Dia 02 - Trabalhando com States e roteamento

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ﻿# Projeto da Imersão React Alura.
 
 ## Nesse Projeto de uma single page application 
-- Ultilizamos a Biblioteca SkynexUI.
-- ultilizamos também o Framework Next.js.
 
 Tecnologias ultilizadas nesse Projeto:
 - HTML/ CSS / JS 
-- REACT JS
+- ReactJs
+- NextJs
+- Biblioteca SkynexUI
 
 ![img](https://github.com/abnerlimaab/aluracord/blob/main/img/Captura%20da%20Web_24-1-2022_22648_localhost.jpeg)
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+import { useRouter } from 'next/router';
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +33,13 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'abnerlimaab';
+  // const username = 'abnerlimaab';
+
+  const [username, setUsername] = React.useState('abnerlimaab');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -91,6 +66,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={
+              function(e) {
+                e.preventDefault()
+                roteamento.push('/chat')
+              }
+            }
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -111,6 +92,11 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
+              onChange={
+                function(event) {
+                  setUsername(event.target.value)
+                }
+              }
             />
             <Button
               type='submit'
